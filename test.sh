e1110a5311241797a072a94916b62297c48fc971
Septatrix <24257556+Septatrix@users.noreply.github.com>
2024-03-31 15:07:59 +0200

    Use --empty=create for sd-repart

--- test.sh
+++ test.sh
@@ -2,14 +2,13 @@
 
 set -eux
 
+export SYSTEMD_LOG_LEVEL=debug
 export SOURCE_DATE_EPOCH="0"
-
-touch image-match a-match b-match
 
 # hash changes from image -> a, and copy from a -> b fails
 systemd-repart \
     --no-pager \
-    --empty=force \
+    --empty=create \
     --size=55M \
     --root=rootfs-match-key \
     --definitions=mkosi.repart/ \
@@ -18,26 +17,24 @@ systemd-repart \
     image-match \
 && sudo --preserve-env=SYSTEMD_LOG_LEVEL,SOURCE_DATE_EPOCH systemd-repart \
     --no-pager \
-    --empty=force \
+    --empty=create \
     --size=55M \
     --image=image-match \
     --dry-run=no \
     a-match \
 && sudo --preserve-env=SYSTEMD_LOG_LEVEL,SOURCE_DATE_EPOCH systemd-repart \
     --no-pager \
-    --empty=force \
+    --empty=create \
     --size=55M \
     --image=a-match \
     --dry-run=no \
     b-match \
 || true
 
-touch image-copy a-copy b-copy
-
 # copy from image -> a fails
 systemd-repart \
     --no-pager \
-    --empty=force \
+    --empty=create \
     --size=55M \
     --root=rootfs-copy-block \
     --definitions=mkosi.repart/ \
@@ -46,14 +43,14 @@ systemd-repart \
     image-copy \
 && sudo --preserve-env=SYSTEMD_LOG_LEVEL,SOURCE_DATE_EPOCH systemd-repart \
     --no-pager \
-    --empty=force \
+    --empty=create \
     --size=55M \
     --image=image-copy \
     --dry-run=no \
     a-copy \
 && sudo --preserve-env=SYSTEMD_LOG_LEVEL,SOURCE_DATE_EPOCH systemd-repart \
     --no-pager \
-    --empty=force \
+    --empty=create \
     --size=55M \
     --image=a-copy \
     --dry-run=no \
